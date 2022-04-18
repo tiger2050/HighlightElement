33fe149b2526f33e3369b99332345ff89ac85000
farhad <fzkhanfred@gmail.com>
2022-04-18 18:01:22 -0300

    new test class NewClass.java has been added

---
+++ src/test/java/com/test/actitime/NewClass.java
@@ -0,0 +1,14 @@
+package com.test.actitime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class NewClass 
+{
+	@Test
+	public void a() 
+	{
+		Reporter.log("a", true);
+	}
+
+}
